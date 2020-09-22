d07101d86dadba7f24212977e0ad98c3b3bf9bdf
shouvikcirca <goldenfibo1123@gmail.com>
2020-09-23 00:39:22 +0530

    added scripts for segregating images into True and False directories

--- scripts/imagegetter.py
+++ scripts/imagegetter.py
@@ -1,19 +1,28 @@
 import matplotlib.pyplot as plt
 import xmlparser
 from PIL import Image
+import os
 
 ents = xmlparser.entries()
 validcount = 0
 invalidcount = 0
 
+di = {} 
 for i in range(len(ents)):
     a = ents[i][1].split('\\')[4:]
+    if a[3] not in di:
+        di[a[3]] = True
     a = '/'.join(a)
     try:
-        img = Image.open(a)
+        b = Image.open(a)
         validcount+=1
     except:
         invalidcount+=1
 
 print('{} valid images'.format(validcount))
 print('{} invalid images'.format(invalidcount))
+
+for item in di:
+    print(item)
+
+print(type(b))
