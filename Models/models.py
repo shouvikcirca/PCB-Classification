22cbcfad553947ebadf3a8829772aea8ecaed355
rhettscronfinkle <goldenfibo1123@gmail.com>
2020-11-07 22:50:42 +0530

    modified models.py

--- Models/models.py
+++ Models/models.py
@@ -1,9 +1,17 @@
-from keras import models
-from keras import layers
-from keras.applications import DenseNet121
+import tensorflow as tf
+if tf.__version__[0] == '2':
+    from tensorflow.keras import models
+    from tensorflow.keras import layers
+    from tensorflow.keras.applications import DenseNet121
 
-def dn121scratch(X):
-    conv_base = DenseNet121(include_top = False, pooling='avg', input_shape=(X.shape[1],X.shape[2],X.shape[3]))
+else:
+    from keras import models
+    from keras import layers
+    from keras.applications import DenseNet121
+
+    
+def dn121scratch():
+    conv_base = DenseNet121(include_top = False, pooling='avg', input_shape=(600,600,3))
     model = models.Sequential()
     model.add(conv_base)
     model.add(layers.Dense(1, activation = 'sigmoid'))
