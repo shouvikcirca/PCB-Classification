e4818a7a64c19bfe7f91c0fa2f9de092e2d3c32a
rhettscronfinkle <goldenfibo1123@gmail.com>
2020-11-18 18:11:40 +0530

    started working on GUI

---
+++ gui/interface.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter.filedialog import *
+from PIL import ImageTk, Image
+
+
+root = Tk()
+root.title('Image Classifier')
+
+def load1():
+    f1 = askopenfilename(title = 'open').split('/')[-1]
+    print(f1)
+    im = Image.open(f1)
+    im = im.resize((250, 250), Image.ANTIALIAS)
+    im = ImageTk.PhotoImage(im)
+    Label(root, image=im).grid(row = 1, column = 0, columnspan = 2)
+    panel.image = im
+ 
+
+
+
+Button(root, text = 'Choose Image', command = load1).grid(row = 0, column = 0)
+Label(root, text = 'Defective').grid(row = 0, column = 1, sticky = E)
+
+for child in root.winfo_children():
+    child.grid_configure(padx = 10, pady = 10)
+
+root.mainloop()
