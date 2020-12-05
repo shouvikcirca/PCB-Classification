ed81331da8f6904656be1f0ae14da49eaf7fd1d0
rhettscronfinkle <goldenfibo1123@gmail.com>
2020-12-05 14:42:01 +0530

    added densenetforaugmix

--- Models/models.py
+++ Models/models.py
@@ -4,17 +4,17 @@ if tf.__version__[0] == '2':
     from tensorflow.keras import models
     from tensorflow.keras import layers
     from tensorflow.keras.applications import DenseNet121
-    import efficientnet.tfkeras as efn
+    #import efficientnet.tfkeras as efn
 
 else:
     from keras import models
     from keras import layers
     from keras.applications import DenseNet121
-    import efficientnet
+    #import efficientnet
     
 def densenet(l):
     if l == 121:
-        conv_base = DenseNet121(include_top = False, pooling='avg', input_shape=(600,600,3))
+        conv_base = DenseNet121(include_top = False, pooling='avg', input_shape=(32,32,3))
     elif l == 169:
         conv_base = DenseNet169(include_top = False, pooling='avg', input_shape=(600,600,3))
     else:
@@ -26,6 +26,22 @@ def densenet(l):
     return model
 
 
+
+def densenetforaugmix(l):
+    if l == 121:
+        conv_base = DenseNet121(include_top = False, pooling='avg', input_shape=(32,32,3))
+    elif l == 169:
+        conv_base = DenseNet169(include_top = False, pooling='avg', input_shape=(600,600,3))
+    else:
+        conv_base = DenseNet201(include_top = False, pooling='avg', input_shape=(600,600,3))
+    
+    model = models.Sequential()
+    model.add(conv_base)
+    model.add(layers.Dense(2, activation = 'softmax'))
+    return model
+
+
+"""
 def efficientnet(v):
     if v == 0:
         conv_base = efn.EfficientNetB0(weights='imagenet',include_top=False, pooling='avg', input_shape=(600,600,3))
@@ -48,3 +64,4 @@ def efficientnet(v):
     model.add(conv_base)
     model.add(layers.Dense(1, activation = 'sigmoid'))
     return model
+"""
